a3abef38dfb948bb53ad4068d2270804411d125e
Isaac Weiland <iweiland7777@gmail.com>
2024-09-23 11:37:46 -0400

    exercise done

--- app.js
+++ app.js
@@ -4,7 +4,29 @@ console.log(
 );
 
 // Exercise 1
-
+let lastName;
+var age;
 // Exercise 2
-
+const firstName = "Isaac";
+lastName = "Weiland";
+age = 23;
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+//Ex4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = false;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+//EX5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+console.log(firstName, lastName, age, language, createdYear, isCaseSensitive)
+console.log(price, isOnSale, salePercentage, stock, inStock, selectedSize)
+console.log(title, author, pageCount, bookmark, hasRead)
